572ef83a9f2c98576b86dbf589bfc07bc1898081
Christopher McCoy <35251302+cmccoy13@users.noreply.github.com>
2018-01-24 10:31:22 -0800

    Update shader_vertex.glsl

--- lab3/resources/shader_vertex.glsl
+++ lab3/resources/shader_vertex.glsl
@@ -7,21 +7,8 @@ uniform mat4 P;
 uniform mat4 V;
 uniform mat4 M;
 uniform vec3 Click;
-//uniform int WindowHeight;
-//uniform int WindowWidth;
 
 void main()
 {
-	/*if(vertPos.z > 0.5)
-	{
-		gl_Position = P * V * M * vec4(Click, 1.0);
-	}*/
-
-	//else
-	//{
-		gl_Position = P * V * M * vec4(vertPos, 1.0);
-	//}
-
 	vCol = vertCol;
-
 }
