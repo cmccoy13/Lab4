aafbac55c51d94c647640d0c73644d7d885437aa
Christopher McCoy <35251302+cmccoy13@users.noreply.github.com>
2018-01-23 23:00:01 -0800

    Update shader_fragment.glsl

--- lab3/resources/shader_fragment.glsl
+++ lab3/resources/shader_fragment.glsl
@@ -2,13 +2,29 @@
 out vec3 color;
 in vec3 vp;
 in vec3 vCol;
+in vec4 gl_FragCoord;
+in vec4 gl_FragColor;
+uniform int WindowHeight;
+uniform int WindowWidth;
+uniform vec4 Center;
 
 void main()
 {
 	color = vCol;
 
-	/*if(distance({320, 240, 0}, vertPos) <= 20)
+	/*if(gl_FragCoord.y > WindowHeight/2)
 	{
 		discard;
 	}*/
+
+	float dist = distance(gl_FragCoord, Center);
+	
+	if(dist < 20)
+	{
+		discard;
+	}
+	else
+	{
+
+	}
 }
